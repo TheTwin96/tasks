d6dc057834b161c65ceeced30190c23e345c433e
TheTwin96 <annielvallevalera96@outlook.com>
2023-03-04 01:17:21 -0600

    Ended project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import freecodecamp_logo from './images/Freecodecamp_Logo.png';
+import Task_List from './components/Task_List';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="task_app">
+      <div className="freecodecamp_logo_container">
+        <img className="freecodecamo_logo" src={freecodecamp_logo} alt="Freecodecamp's logo" />
+      </div>
+      <div className="task_main_list">
+        <h1>Mis Tareas</h1>
+        <Task_List />
+      </div>
     </div>
   );
 }
